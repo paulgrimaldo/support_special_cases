69c55484e06272aa3a033feaf0dfb7ff888a7b60
Paul Grimaldo <paulgrimaldobravo@gmail.com>
2020-07-11 17:43:32 -0400

    Fixed production host

--- app_frontend/config/prod.env.js
+++ app_frontend/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  GET_CAREERS_API_URL: '"http://localhost/api/v1/careers"',
-  GENERATE_DOCUMENTATION_API_URL: '"http://localhost/api/v1/generate-documentation"'
+  GET_CAREERS_API_URL: '"http://3.134.117.12/api/v1/careers"',
+  GENERATE_DOCUMENTATION_API_URL: '"http://3.134.117.12/api/v1/generate-documentation"'
 }
